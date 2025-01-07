b39d2df18a7d02f1b7d4a962598f5a51f7ac9580
irfan.putra <irfan.putra@superbank.id>
2025-01-07 03:39:36 +0000

    add test scenario

--- test/lazydocker-binary-release/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Optional: Import test library bundled with the devcontainer CLI
-source dev-container-features-test-lib
-
-# Feature-specific tests
-# The 'check' command comes from the dev-container-features-test-lib.
-check "lazydocker" lazydocker --version
